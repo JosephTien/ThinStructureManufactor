ed3e6904382b2c4d81abb47d4c7d1dd4c00f016c
Joseph <joseph811223@gmail.com>
2017-04-05 11:58:37 +0800

    2017/04/05 11:50 complete avaliable-vectors and neighborInfo

--- assemblyinfo.h
+++ assemblyinfo.h
@@ -1,16 +1,21 @@
 #ifndef ASSEMBLYINFO_H
 #define ASSEMBLYINFO_H
 #include<pch.h>
+#define ALMOSTONE 0.97f
+#define ALMOSTZERO 0.2f
 class AsmComp{
 public:
-    AsmComp(int edge, QVector3D splitNorm, QVector3D asmDir){
+    AsmComp(int edge, QVector3D v1, QVector3D v2){
         this->edge = edge;
-        this->splitNorm = splitNorm;
-        this->asmDir = asmDir;
+        this->v1 = v1;
+        this->v2 = v2;
+        this->direction = (v2-v1).normalized();
     }
+    constexpr static float flexAngle = 45.0f;
     int edge;
-    QVector3D splitNorm;
-    QVector3D asmDir;
+    QVector3D v2;
+    QVector3D v1;
+    QVector3D direction;
     bool operator < (const AsmComp& asmComp) const
     {
         return this->edge < asmComp.edge;
@@ -19,26 +24,198 @@ public:
     {
         return this->edge == asmComp.edge;
     }
+    bool operator != (const AsmComp& asmComp) const
+    {
+        return this->edge != asmComp.edge;
+    }
     bool operator > (const AsmComp& asmComp) const
     {
         return this->edge > asmComp.edge;
     }
 };
 
-class AsmSet : std::set<AsmComp>{
+class AsmTerm{
+public:
+    constexpr static int div = 36;
+    AsmTerm(){}
+    std::vector<QVector3D> restrictVecs;
+    //std::vector<float> restrictAngles;
+    std::vector<QVector3D> avaliableVecs;
+
+    void creatAvaliableVecs();
+
+    void cleanAvaliableVecs();
+
+    void addRestrict(QVector3D restrictVec, float restrictAngle){
+        restrictVecs.push_back(restrictVec.normalized()*sin(restrictAngle/M_PI*180));
+        //restrictAngles.push_back(restrictAngle);
+        creatAvaliableVecs();
+    }
+
+    float dot(QVector3D a, QVector3D b)const{
+        return QVector3D::dotProduct(a.normalized(), b.normalized());
+    }
+
+    QVector3D cross(QVector3D a, QVector3D b)const{
+        return QVector3D::crossProduct(a.normalized(), b.normalized());
+    }
+
+    int size() const{
+        return restrictVecs.size();
+    }
+
+    AsmTerm& operator = (const AsmTerm& asmTerm){
+        this->restrictVecs = asmTerm.restrictVecs;
+        //this->restrictAngles = asmTerm.restrictAngles;
+        return *this;
+    }
+    AsmTerm& operator += (const AsmTerm& asmTerm){
+        for(int i=0;i<asmTerm.size();i++){
+            this->restrictVecs.push_back(asmTerm.restrictVecs[i]);
+            //this->restrictAngles.push_back(asmTerm.restrictAngles[i]);
+        }
+        return *this;
+    }
+
+    AsmTerm operator + (const AsmTerm& asmTermR) const{
+        return (AsmTerm()=*this) += asmTermR;
+    }
+};
+
+class neighborEdgeInfo{
+public:
+    neighborEdgeInfo(int neighborEdge, int parentVertice, std::set<int> parentEdges){
+        this->neighborEdge = neighborEdge;
+        this->parentVertice = parentVertice;
+        this->parentEdges = parentEdges;
+    }
+    int neighborEdge;
+    int parentVertice;
+    std::set<int> parentEdges;
+    bool operator < (const neighborEdgeInfo& info) const
+    {
+        if(this->neighborEdge == info.neighborEdge)
+            return this->parentVertice < info.parentVertice;
+        return this->neighborEdge < info.neighborEdge;
+    }
+    bool operator == (const neighborEdgeInfo& info) const
+    {
+        if(this->neighborEdge == info.neighborEdge)
+            return this->parentVertice == info.parentVertice;
+        return this->neighborEdge == info.neighborEdge;
+    }
+    bool operator != (const neighborEdgeInfo& info) const
+    {
+        if(this->neighborEdge == info.neighborEdge)
+            return this->parentVertice != info.parentVertice;
+        return this->neighborEdge != info.neighborEdge;
+    }
+    bool operator > (const neighborEdgeInfo& info) const
+    {
+        if(this->neighborEdge == info.neighborEdge)
+            return this->parentVertice > info.parentVertice;
+        return this->neighborEdge > info.neighborEdge;
+    }
+};
+class AsmSet{
 public:
     AsmSet(){}
-    AsmSet(std::set<AsmComp> asmComp){(*this) = asmComp;}
-    bool add(int edge, QVector3D splitNorm, QVector3D asmDir){
-        AsmComp asmComp = AsmComp(edge, splitNorm, asmDir);
-        if(this->count(asmComp)>0){
-            this->erase(asmComp);
-            this->insert(asmComp);
+    AsmSet(std::set<AsmComp> asmSet){(*this) = asmSet;}
+    std::set<AsmComp> asmComps;
+    AsmTerm asmTerm;
+    std::set<neighborEdgeInfo> infos;
+    /*
+    std::vector<int> neighborEdges;
+    std::vector<int> parentVertices;
+    std::vector<std::set<int>> parentEdges;
+    */
+
+    bool add(AsmComp asmComp){
+        if(asmComps.count(asmComp)>0){
+            asmComps.erase(asmComp);
+            asmComps.insert(asmComp);
             return true;
         }else{
-            this->insert(asmComp);
+            asmComps.insert(asmComp);
             return false;
         }
+    }
+
+    bool add(int edge, QVector3D v1, QVector3D v2){
+        AsmComp asmComp = AsmComp(edge, v1, v2);
+        if(asmComps.count(asmComp)>0){
+            asmComps.erase(asmComp);
+            asmComps.insert(asmComp);
+            return true;
+        }else{
+            asmComps.insert(asmComp);
+            return false;
+        }
+    }
+    std::vector<AsmComp> getAsmCompsVector() const
+    {
+        std::vector<AsmComp> asmCompsVector;
+        for(auto asmComp:asmComps){
+            asmCompsVector.push_back(asmComp);
+        }
+        return asmCompsVector;
+    }
+    bool operator < (const AsmSet& asmSet) const
+    {
+        std::vector<AsmComp> thisSet = this->getAsmCompsVector();
+        std::vector<AsmComp> thatSet = asmSet.getAsmCompsVector();
+        if(thisSet.size()<thatSet.size())return true;
+        if(thisSet.size()>thatSet.size())return false;
+        int ss = thisSet.size();
+        for(int i=0;i<ss;i++){
+            if(thisSet[i]<thatSet[i])return true;
+            if(thisSet[i]>thatSet[i])return false;
+        }
+        return false;
+    }
+
+    bool operator == (const AsmSet& asmSet) const
+    {
+        std::vector<AsmComp> thisSet = this->getAsmCompsVector();
+        std::vector<AsmComp> thatSet = asmSet.getAsmCompsVector();
+        if(thisSet.size()<thatSet.size())return false;
+        if(thisSet.size()>thatSet.size())return false;
+        int ss = thisSet.size();
+        for(int i=0;i<ss;i++){
+            if(thisSet[i]!=thatSet[i])return false;
+        }
+        return true;
+    }
+    bool operator > (const AsmSet& asmSet) const
+    {
+        std::vector<AsmComp> thisSet = this->getAsmCompsVector();
+        std::vector<AsmComp> thatSet = asmSet.getAsmCompsVector();
+        if(thisSet.size()<thatSet.size())return false;
+        if(thisSet.size()>thatSet.size())return true;
+        int ss = thisSet.size();
+        for(int i=0;i<ss;i++){
+            if(thisSet[i]<thatSet[i])return false;
+            if(thisSet[i]>thatSet[i])return true;
+        }
+        return false;
+    }
+
+    AsmSet& operator = (const AsmSet& asmSet)
+    {
+        this->asmComps = asmSet.asmComps;
+        this->asmTerm = asmSet.asmTerm;
+        return *this;
+    }
+
+    AsmSet& operator += (const AsmSet& asmSet){
+        this->asmTerm+=asmSet.asmTerm;
+        for(auto asmComp : asmSet.asmComps){
+            this->add(asmComp);
+        }
+        return *this;
+    }
+    AsmSet operator + (const AsmSet& asmSetR)const{
+        return AsmSet(*this)+=asmSetR;
     }
 };
 class AssemblyInfo
@@ -49,15 +226,18 @@ public:
 
     std::vector<float> vertices;
     std::vector<int> edges;
+    std::vector<std::set<int>> neighbors;
+    std::vector<std::vector<int>> edgesMap;
     std::vector<AsmSet> asmSets;
     std::vector<int> asmSetsPrefix;
     std::vector<int> combination;
 
-
+    inline int getVerticeNum(){return vertices.size()/3;}
     inline int getEdgeNum(){return edges.size()/2;}
     inline QVector3D getVertice(int idx){return QVector3D(vertices[idx*3],vertices[idx*3+1],vertices[idx*3+2]);}
     inline QVector3D getEdgeIdx(int idx, int &v1idx, int &v2idx){v1idx = edges[idx*2];v2idx = edges[idx*2+1];}
     inline QVector3D getEdge(int idx, QVector3D &v1, QVector3D &v2){v1 = getVertice(edges[idx*2]);v2 = getVertice(edges[idx*2+1]);}
+    std::set<neighborEdgeInfo> getAsmSetNeighborEdges(int idx);
     void calAsmSets();
     void calCombination();
 
--- code-others/assemblyinfo.cpp
+++ code-others/assemblyinfo.cpp
@@ -1,5 +1,43 @@
 #include "assemblyinfo.h"
+void AsmTerm::creatAvaliableVecs(){
+    avaliableVecs.clear();
+    int thdiv = div*4;
+    for(int theta=0;theta<thdiv;theta++){
+        avaliableVecs.push_back(QVector3D(cos(2*M_PI*theta/div), sin(2*M_PI*theta/div), 0));
+    }
+    for(int phi=1;phi<=div;phi++){
+        float phsin = sin(M_PI/2*phi/div);
+        float phcos = cos(M_PI/2*phi/div);
+        int thdiv = div*phcos*4+1;
+        for(int theta=0;theta<thdiv;theta++){
+            avaliableVecs.push_back(QVector3D(phcos*cos(2*M_PI*theta/thdiv), phcos*sin(2*M_PI*theta/thdiv), phsin));
+        }
+        for(int theta=0;theta<thdiv;theta++){
+            avaliableVecs.push_back(QVector3D(phcos*cos(2*M_PI*theta/thdiv), phcos*sin(2*M_PI*theta/thdiv), -phsin));
+        }
+    }
+    cleanAvaliableVecs();
+}
 
+void AsmTerm::cleanAvaliableVecs(){
+    std::vector<QVector3D> _avaliableVecs;
+    for(int i=0;i<avaliableVecs.size();i++){
+        QVector3D avaliableVec = avaliableVecs[i];
+        bool nvalid=false;
+        for(int j=0;j<restrictVecs.size();j++){
+            QVector3D restrictVec = restrictVecs[j];
+            if(dot(avaliableVec-restrictVec,restrictVec)>0){
+                nvalid = true;
+                break;
+            }
+        }
+        if(!nvalid){
+            _avaliableVecs.push_back(avaliableVec);
+        }
+    }
+    avaliableVecs = _avaliableVecs;
+}
+//---------------------------------------------------------
 AssemblyInfo::AssemblyInfo()
 {
 
@@ -8,9 +46,88 @@ AssemblyInfo::AssemblyInfo()
 AssemblyInfo::AssemblyInfo(std::vector<float> vertices, std::vector<int> edges){
     this->vertices = vertices;
     this->edges = edges;
+    neighbors.resize(getVerticeNum(),std::set<int>());
+    //std::vector<int> pushor;pushor.resize(getVerticeNum(),-1);
+    //edgesMap.resize(getVerticeNum(),pushor);
+    for(int i=0;i<getVerticeNum();i++){
+        edgesMap.push_back(std::vector<int>());
+        for(int j=0;j<getVerticeNum();j++){
+            edgesMap[i].push_back(-1);
+        }
+    }
+    for(int i=0;i<getEdgeNum();i++){
+        neighbors[edges[i*2]].insert(edges[i*2+1]);
+        neighbors[edges[i*2+1]].insert(edges[i*2]);
+        edgesMap[edges[i*2]][edges[i*2+1]]=i;
+        edgesMap[edges[i*2+1]][edges[i*2]]=i;
+    }
+}
+
+std::set<neighborEdgeInfo> AssemblyInfo::getAsmSetNeighborEdges(int idx){
+    std::set<int> neighborEdges;
+    std::vector<int> froms;
+    std::vector<int> froms2;
+    std::vector<std::set<int>> fromedges;
+    froms.resize(this->getEdgeNum(),-1);
+    froms2.resize(this->getEdgeNum(),-1);
+    fromedges.resize(this->getEdgeNum(),std::set<int>());
+    for(auto asmComp : asmSets[idx].asmComps){
+        int edge = asmComp.edge;
+        int v1idx = edges[edge*2];
+        int v2idx = edges[edge*2+1];
+        for(auto vert:neighbors[v1idx]){
+            int vidx = v1idx;
+            int thatedge = edgesMap[vidx][vert];
+            neighborEdges.insert(thatedge);
+            if(froms[thatedge]!=-1&&froms[thatedge]!=vidx)froms2[thatedge] = vidx;
+            else froms[thatedge] = vidx;
+            fromedges[thatedge].insert(edge);
+        }
+        for(auto vert:neighbors[v2idx]){
+            int vidx = v2idx;
+            int thatedge = edgesMap[vidx][vert];
+            neighborEdges.insert(thatedge);
+            if(froms[thatedge]!=-1&&froms[thatedge]!=vidx)froms2[thatedge] = vidx;
+            else froms[thatedge] = vidx;
+            fromedges[thatedge].insert(edge);
+        }
+    }
+    for(auto asmComp : asmSets[idx].asmComps){
+        int edge = asmComp.edge;
+        neighborEdges.erase(edge);
+    }
+    std::set<neighborEdgeInfo> infos;
+    infos.clear();
+    for(auto edge:neighborEdges){
+        infos.insert(neighborEdgeInfo(edge ,froms[edge], fromedges[edge]));
+        if(froms2[edge]!=-1){
+            infos.insert(neighborEdgeInfo(edge ,froms2[edge], fromedges[edge]));
+        }
+    }
+    return infos;
 }
 
 void AssemblyInfo::calAsmSets(){
+    for(int i=0;i<getEdgeNum();i++){
+        QVector3D v1,v2;
+        getEdge(i,v1,v2);
+        asmSets.push_back(AsmSet());
+        asmSets[i].add(i,v1,v2);
+        asmSets[i].asmTerm.addRestrict((v2-v1).normalized(), AsmComp::flexAngle);
+        asmSets[i].asmTerm.addRestrict((v1-v2).normalized(), AsmComp::flexAngle);
+        asmSets[i].infos = getAsmSetNeighborEdges(i);
+        /*
+        for(auto e : asmSets[i].infos){
+            qDebug() << e.neighborEdge;
+            qDebug() << " " << e.parentVertice;
+            for(auto es : e.parentEdges){
+                qDebug() << "  " << es;
+            }
+        }
+        qDebug() << "";
+        */
+
+    }
 
 }
 
